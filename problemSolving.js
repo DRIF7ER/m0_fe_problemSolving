ee4265c0fe3e4977ca572d91b03eb0bbd21a6131
Charles B. <charlesbanks238@gmail.com>
2024-03-11 05:47:15 -0700

    Adds code specified in 'CHALLENGE 2'.

--- problemSolving.js
+++ problemSolving.js
@@ -16,3 +16,22 @@ function testWordEvenOrOdd(string){;
    console.log(
    
    )
+// CHALLENGE 2
+   
+// Update your previous method or function so it accepts an array. The method or function
+// should return a boolean that describes whether or not the array's length is an even number.
+   
+function testListEvenOrOdd(...input){;
+    var list = input;
+    if (list.length % 2 === 0){;
+        console.log("This list is even.")};
+    if (input.length % 2 !== 0){;
+        console.log("This list is odd.")};
+    };
+   
+testListEvenOrOdd("eggs", "cheese", "milk", "beer")
+testListEvenOrOdd("Halo", "Division", "Warframe")
+// FOR ME: solved using an array as a parameter but using '...' before my declaration.
+console.log(
+
+)
