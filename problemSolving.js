553150f627136ca36c705409b99c602844df6a9f
Charles B. <charlesbanks238@gmail.com>
2024-03-11 05:45:15 -0700

    Adds code specified in 'CHALLENGE 1'.

---
+++ problemSolving.js
@@ -0,0 +1,18 @@
+// CHALLENGE 1
+
+// Write a method or function that accepts a string. The method or function should return a boolean that describes whether or not
+// the string has an even number of characters.
+
+function testWordEvenOrOdd(string){;
+    var word = string;
+    if (word.length % 2 === 0){;
+       console.log(`The word ${string} is even.`)};
+    if (word.length % 2 !== 0){;
+       console.log(`The word ${string} is odd.`)};
+   };
+   
+   testWordEvenOrOdd("antidisestablishmentarianism")
+   testWordEvenOrOdd("shwifty")
+   console.log(
+   
+   )
